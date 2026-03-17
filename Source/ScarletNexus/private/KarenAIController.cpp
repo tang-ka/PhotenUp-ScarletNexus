859a32c71a88beba5df14f9e6797c3670ba1b9ae
ojaejun1995-sys <ojaejun1995@gmail.com>
2026-03-18 07:30:28 +0900

    Statetree 시작

---
+++ Source/ScarletNexus/private/KarenAIController.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KarenAIController.h"
+#include "Components/StateTreeComponent.h"
+
+AKarenAIController::AKarenAIController()
+{
+	StateTreeComponent = CreateDefaultSubobject<UStateTreeComponent>(TEXT("StateTreeComponent"));
+	
+}
---
+++ Source/ScarletNexus/private/KarenTeleportTask.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KarenTeleportTask.h"
+
+KarenTeleportTask::KarenTeleportTask()
+{
+	
+}
+
+KarenTeleportTask::~KarenTeleportTask()
+{
+	
+}
