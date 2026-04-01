17554bed25510d2b3dd2caf276fa5d34c4711d41
zpfhfh0124 <yuki79000@gmail.com>
2026-04-01 20:14:54 +0900

    적 AI Controller 생성

--- Source/ScarletNexus/Public/Enemy/EnemyAIController.h
+++ Source/ScarletNexus/Public/Enemy/EnemyAIController.h
@@ -14,4 +14,31 @@ class SCARLETNEXUS_API AEnemyAIController : public AAIController
 {
 	GENERATED_BODY()
 	
+public:
+	AEnemyAIController();
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void OnPossess(APawn* Pawn) override;
+	virtual void OnUnPossess() override;
+	
+private:
+	// AIPerception : 시야로 플레이어 감지
+	UPROPERTY(VisibleAnywhere, Category="AI")
+	TObjectPtr<UAIPerceptionComponent> AIPerceptionComp;
+	
+	UPROPERTY(VisibleAnywhere, Category="AI")
+	TObjectPtr<class UAISenseConfig_Sight> SightConfig;
+	
+	// State Tree 콤포넌트 (Pawn에 붙이지 않고 Controller에서 관리)
+	UPROPERTY(VisibleAnywhere, Category="AI")
+	TObjectPtr<class UStateTreeComponent> StateTreeComp;
+	
+	// 에디터에서 할당할 StateTree 에셋
+	UPROPERTY(EditDefaultsOnly, Category="AI")
+	TObjectPtr<class UStateTree> EnemyStateTree;
+	
+	// 감지 이벤트 콜백
+	UFUNCTION()
+	void OnTargetPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
 };
