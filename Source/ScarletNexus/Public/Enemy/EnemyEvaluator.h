a5677bcf45c8d6e4ed45a0d283848a9775ff0008
zpfhfh0124 <yuki79000@gmail.com>
2026-04-02 15:59:43 +0900

    Enemy Task 생성

--- Source/ScarletNexus/Public/Enemy/EnemyEvaluator.h
+++ Source/ScarletNexus/Public/Enemy/EnemyEvaluator.h
@@ -3,11 +3,45 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "StateTreeEvaluatorBase.h"
 
-/**
- * 
- */
-class SCARLETNEXUS_API EnemyEvaluator
+USTRUCT()
+struct FEnemyEvaluatorInstanceData
 {
+	GENERATED_BODY()
+	
+	// 최근접 타겟 (플레이어 or 파티맴버)
+	UPROPERTY(EditAnywhere, meta=(Output))
+	TObjectPtr<AActor> NearestTarget = nullptr;
+	
+	// 탐지 반경
+	UPROPERTY(EditAnywhere)
+	float DetectRadius = 1500.f;
+	
+	// 공격 사거리
+	UPROPERTY(EditAnywhere)
+	float AttackRange = 200.f;
+	
+	// 타겟이 공격 범위 안에 있는지 체크
+	UPROPERTY(EditAnywhere, meta=(Output))
+	bool bInAttackRange = false;
+	
+	// 타겟까지 거리
+	UPROPERTY(EditAnywhere, meta=(Output))
+	float DistanceToTarget = 0.f;
+};
 
+USTRUCT(DisplayName="Enemy Evaluator")
+struct FEnemyEvaluator : public FStateTreeEvaluatorBase
+{
+	GENERATED_BODY()
+	using FInstanceDataType = FEnemyEvaluatorInstanceData;
+	
+	virtual const UStruct* GetInstanceDataType() const override
+	{
+		return FInstanceDataType::StaticStruct();
+	}
+	
+	virtual void TreeStart(FStateTreeExecutionContext& Context) const override;
+	virtual void Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const override;
 };
---
+++ Source/ScarletNexus/Public/Enemy/EnemyTask_Chase.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StateTreeTaskBase.h"
+
+USTRUCT()
+struct FEnemyTask_ChaseInstanceData
+{
+	GENERATED_BODY()
+	
+	// Evaluator에서 받아온 추적 대상
+	UPROPERTY(EditAnywhere, mata = (Input))
+	TObjectPtr<AActor> ChaseTarget = nullptr;
+	
+	// 도착 판정 거리
+	UPROPERTY(EditAnywhere)
+	float AcceptanceRadius = 150.f;
+	
+	// MoveTo 갱신 주기 (초) : 갱신 딜레이
+	UPROPERTY(EditAnywhere)
+	float RepathInterval = 0.3f;
+	float ElapsedSinceRepath = 0.f;
+};
+
+USTRUCT(DisplayName="Enemy: Chase Target")
+struct FEnemyTask_Chase : public FStateTreeTaskBase
+{
+	GENERATED_BODY()
+	using FInstanceDataType = FEnemyTask_ChaseInstanceData;
+	
+	virtual const UStruct* GetInstanceDataType() const override
+	{
+		return FInstanceDataType::StaticStruct();
+	}
+	
+	virtual EStateTreeRunStatus EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const override;
+	virtual EStateTreeRunStatus Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const override;
+	virtual void ExitState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const override;
+};
