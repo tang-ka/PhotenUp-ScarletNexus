6d6294da58be3b9d4f6e64550157f08475c52998
ojaejun1995-sys <ojaejun1995@gmail.com>
2026-04-08 17:22:52 +0900

    콜리전설정

--- Source/ScarletNexus/Public/Boss/BossAttackCollisionComponent.h
+++ Source/ScarletNexus/Public/Boss/BossAttackCollisionComponent.h
@@ -2,43 +2,43 @@
 
 #pragma once
 
+#include <Enemy/EnemyAttackCollision.h>
 #include "CoreMinimal.h"
-#include "Components/SphereComponent.h"
 #include "BossAttackCollisionComponent.generated.h"
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
-class SCARLETNEXUS_API UBossAttackCollisionComponent : public USphereComponent
+class SCARLETNEXUS_API UBossAttackCollisionComponent : public UEnemyAttackCollision
 {
-	GENERATED_BODY()
-
-public:
-	UBossAttackCollisionComponent();
-
-	// 콜리전 활성화 (데미지 판정 시작)
-	void EnableAttackCollision(float InDamage, float InKnockback = 0.f);
-
-	// 콜리전 비활성화 (데미지 판정 종료)
-	void DisableAttackCollision();
-
-	// 이번 공격에서 이미 히트한 액터 초기화
-	void ResetHitActors();
-
-protected:
-	virtual void BeginPlay() override;
-
-	UFUNCTION()
-	void OnAttackOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
-		bool bFromSweep, const FHitResult& SweepResult);
-
-private:
-	// 현재 공격의 데미지
-	float CurrentDamage = 0.f;
-
-	// 현재 공격의 넉백
-	float CurrentKnockback = 0.f;
-
-	// 이번 공격에서 이미 히트한 액터 목록 (중복 데미지 방지)
-	UPROPERTY()
-	TArray<TObjectPtr<AActor>> HitActors;
+ 	GENERATED_BODY()
+//
+// public:
+// 	UBossAttackCollisionComponent();
+//
+// 	// 콜리전 활성화 (데미지 판정 시작)
+// 	void EnableAttackCollision(float InDamage, float InKnockback = 0.f);
+//
+// 	// 콜리전 비활성화 (데미지 판정 종료)
+// 	void DisableAttackCollision();
+//
+// 	// 이번 공격에서 이미 히트한 액터 초기화
+// 	void ResetHitActors();
+//
+// protected:
+// 	virtual void BeginPlay() override;
+//
+// 	UFUNCTION()
+// 	void OnAttackOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+// 		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+// 		bool bFromSweep, const FHitResult& SweepResult);
+//
+// private:
+// 	// 현재 공격의 데미지
+// 	float CurrentDamage = 0.f;
+//
+// 	// 현재 공격의 넉백
+// 	float CurrentKnockback = 0.f;
+//
+// 	// 이번 공격에서 이미 히트한 액터 목록 (중복 데미지 방지)
+// 	UPROPERTY()
+// 	TArray<TObjectPtr<AActor>> HitActors;
 };
